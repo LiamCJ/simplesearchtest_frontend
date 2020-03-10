61058b1c007727eb4e8b63185565dc3968d59044
LiamCJ <liamcjones2000@gmail.com>
2020-03-10 23:55:59 +0200

    Javascript file setting the routes of The Gauntlet

--- TheGauntlet_ClientApp/src/App.js
+++ TheGauntlet_ClientApp/src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './App.css';
 import Landing from './components/index';
 import StatePage from './components/state_page';
-import { Route, HashRouter as Router} from 'react-router-dom'
+import { Route, BrowserRouter as Router} from 'react-router-dom'
 
 class App extends React.Component {
     render() {
         return (
             <React.Fragment>
+                {/* A Browser Router containing two routes */}
                     <Router>
+                        {/* Route for the Landing page */}
                         <Route exact path="/" component={Landing} />
-                        <Route path="/StatePage" component={StatePage} />
+                        {/* Route for the State Pages */}
+                        <Route path="/:StateId" component={StatePage} />
                     </Router>   
             </React.Fragment>
         )
